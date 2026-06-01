2a11b8ab3d1eaaba0547c00c1b4a52aa1cc1a729
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-06-01 20:30:33 +0700

    menunjuk ke sebuah nama file dengan ekstentsi txt

--- IOfilewithpath.cpp
+++ IOfilewithpath.cpp
@@ -9,14 +9,13 @@ int main()
     string baris, NamaFile;
 
     //membuat input untuk menangkap nama file
-   
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
 
     //membuat file dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file dengan ekstentsi txt
-    outfile.open(NamaFile + ".txt", ios::out);
-
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
 
     //looping untuk menulis isi file
     while (true)
