5e41faba2dc30c6447b876ef1fdceb97e84888b6
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-06-01 20:26:53 +0700

    menulis dan memasukkan nilai dari variabel baris ke dalam file

--- streamreader.cpp
+++ streamreader.cpp
@@ -22,9 +22,8 @@ int main()
         //mendapat setiap karakter dalam satu baris
         getline(cin, baris);
         //kondisi false jika baris hanya berisi karakter q
-        
+        if (baris == "q") break;
         //menulis dan memasukkan nilai dari variabel baris ke dalam file
-        outfile << baris << endl;
 
     }
     //menutup file setelah penulisan selesai
